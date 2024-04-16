124d46f3aece63e316aefa3639f96909e7212704
ByteZ1337 <0xbytez@gmail.com>
2024-04-16 12:02:59 +0200

    Let read_ext use ByteOrder functions

--- src/read_ext.rs
+++ src/read_ext.rs
@@ -1,30 +1,41 @@
-use std::io::{Cursor, Read};
+use std::io::Read;
+
+use byteorder::{BigEndian, ByteOrder, LittleEndian, ReadBytesExt};
+
+use crate::unity::util::Endian;
 
 pub trait ReadExt {
-    fn read_i16(&mut self) -> i16;
-    
-    fn read_u64(&mut self) -> u64;
+    fn read_string(&mut self, len: usize) -> anyhow::Result<String>;
 
-    fn read_string(&mut self) -> String;
+    fn read_size<T: ByteOrder>(&mut self, size_bits: u32) -> anyhow::Result<usize>;
+
+    fn read_dyn_string(&mut self, endian: &Endian, size_bits: u32) -> anyhow::Result<String>;
 }
 
-impl<T: AsRef<[u8]>> ReadExt for Cursor<T> {
-    fn read_i16(&mut self) -> i16 {
-        let mut buf = [0; 2];
-        self.read_exact(&mut buf).unwrap();
-        i16::from_le_bytes(buf)
-    }
-
-    fn read_u64(&mut self) -> u64 {
-        let mut buf = [0; 8];
-        self.read_exact(&mut buf).unwrap();
-        u64::from_le_bytes(buf)
-    }
-
-    fn read_string(&mut self) -> String {
-        let len = self.read_i16() as usize;
+impl<R: Read + ?Sized> ReadExt for R {
+    fn read_string(&mut self, len: usize) -> anyhow::Result<String> {
         let mut buf = vec![0; len];
-        self.read_exact(&mut buf).unwrap();
-        String::from_utf8(buf).unwrap()
+        self.read_exact(&mut buf)
+            .map_err(|e| anyhow::anyhow!("Failed to read string: {}", e))?;
+        String::from_utf8(buf)
+            .map_err(|e| anyhow::anyhow!("Failed to convert string to utf8: {}", e))
+    }
+
+    fn read_size<T: ByteOrder>(&mut self, size_bits: u32) -> anyhow::Result<usize> {
+        match size_bits {
+            8 => self.read_u8().map(|v| v as usize),
+            16 => self.read_u16::<T>().map(|v| v as usize),
+            32 => self.read_u32::<T>().map(|v| v as usize),
+            _ => return Err(anyhow::anyhow!("Invalid size bits: {}", size_bits)),
+        }.map_err(|e| anyhow::anyhow!("Failed to read size: {}", e))
+    }
+
+    fn read_dyn_string(&mut self, endian: &Endian, size_bits: u32) -> anyhow::Result<String> {
+        let size = match endian {
+            Endian::Little => self.read_size::<LittleEndian>(size_bits),
+            Endian::Big => self.read_size::<BigEndian>(size_bits),
+        }?;
+
+        self.read_string(size)
     }
 }
